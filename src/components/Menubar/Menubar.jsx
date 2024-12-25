869703a7cb717b6addf8bf0119072ce12f82f4d3
jinkazama13200 <jinkazama13200@gmail.com>
2024-12-25 18:35:23 +0700

    fixed router dom link

--- src/components/Menubar/Menubar.jsx
+++ src/components/Menubar/Menubar.jsx
@@ -1,12 +1,12 @@
 import { buttonStyles } from "../Button/Button";
 import { twMerge } from "tailwind-merge";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Film, Home, Users } from "lucide-react";
 
 const Menubar = () => {
   return (
     <div className="fixed bottom-5 left-1/2 flex -translate-x-[50%] items-center justify-center gap-2 rounded-full bg-white p-2 shadow-md lg:hidden">
-      <MenubarItem Icon={Home} url={"/"} />
+      <MenubarItem Icon={Home} url="/" />
       <MenubarItem Icon={Film} url="/movies" />
       <MenubarItem Icon={Users} url="/users" />
     </div>
@@ -15,18 +15,17 @@ const Menubar = () => {
 
 const MenubarItem = ({ Icon, title, url }) => {
   const location = useLocation();
-  const navigate = useNavigate();
   return (
-    <a
+    <Link
+      to={`${url}`}
       className={twMerge(
         buttonStyles({ variant: "ghost", size: "icon" }),
         `flex w-full items-center shadow-md ${url === location.pathname ? "bg-secondary-default text-white shadow-secondary-default" : undefined} `,
       )}
-      onClick={() => navigate(url)}
     >
       <Icon className="h-5 w-5" />
       <div className="text-sm">{title}</div>
-    </a>
+    </Link>
   );
 };
 
