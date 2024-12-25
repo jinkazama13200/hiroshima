3ea21eb46cb9f57006f09cb9fe3558dc9058c65c
jinkazama13200 <jinkazama13200@gmail.com>
2024-12-25 18:56:39 +0700

    fixed 3rd router dom link

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,15 @@
 import { Film, Home, Users } from "lucide-react";
 import { buttonStyles } from "../Button/Button";
 import { twMerge } from "tailwind-merge";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <>
       <aside className="top-0 hidden h-screen w-56 flex-col gap-2 overflow-y-auto border border-r p-2 lg:sticky lg:flex">
         <LargeSidebarItem isActive Icon={Home} title="home" url="/" />
-        <LargeSidebarItem Icon={Film} title="movies management" url="/movies" />
-        <LargeSidebarItem Icon={Users} title="users management" url="/users" />
+        <LargeSidebarItem Icon={Film} title="movies management" url="movies" />
+        <LargeSidebarItem Icon={Users} title="users management" url="users" />
       </aside>
     </>
   );
@@ -19,18 +19,18 @@ const LargeSidebarItem = ({ Icon, title, url }) => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <a
+    <Link
+      to={url}
       className={twMerge(
         buttonStyles({ variant: "ghost" }),
         `flex gap-2 rounded-md px-5 ${url === location.pathname ? "bg-secondary-default text-white" : undefined} `,
       )}
-      onClick={() => navigate(url)}
     >
       <Icon className="h-6 w-6" />
       <div className="overflow-hidden text-ellipsis whitespace-nowrap">
         {title}
       </div>
-    </a>
+    </Link>
   );
 };
 
