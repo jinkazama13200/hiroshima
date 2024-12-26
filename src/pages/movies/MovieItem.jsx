6ae4f53bd75ea98311d62313bd9bee62304d08bc
jinkazama13200 <jinkazama13200@gmail.com>
2024-12-26 20:05:10 +0700

    added npage for movies page

--- src/pages/movies/MovieItem.jsx
+++ src/pages/movies/MovieItem.jsx
@@ -4,7 +4,7 @@ import Button from "../../components/Button/Button";
 
 const MovieItem = ({ hinhAnh, maPhim, biDanh, tenPhim }) => {
   return (
-    <div className="flex items-center justify-center gap-2 overflow-hidden rounded-md px-3 py-1 shadow-md lg:gap-4 hover:bg-gray-300 transition-colors ">
+    <div className="flex items-center justify-center gap-2 overflow-hidden rounded-md px-3 py-1 shadow-md transition-colors hover:bg-gray-300 lg:gap-4">
       <img
         className="rounded-md"
         width={100}
@@ -12,18 +12,20 @@ const MovieItem = ({ hinhAnh, maPhim, biDanh, tenPhim }) => {
         src={hinhAnh}
         alt={biDanh}
       />
-      <div className="flex flex-1 flex-col gap-2 overflow-hidden lg:gap-4">
-        <span className="inline-block w-full rounded-md bg-secondary-default p-2 text-white">
-          {maPhim}
-        </span>
+      <div className="flex flex-1 flex-col gap-2 overflow-hidden py-2 lg:gap-4">
+        <p>
+          <span className="rounded-md bg-secondary-default p-2 text-white">
+            {maPhim}
+          </span>
+        </p>
         <h3 className="overflow-hidden truncate whitespace-nowrap">
           {tenPhim}
         </h3>
-        <div className="flex items-center justify-start gap-2 lg:gap-4">
-          <Button>
+        <div className="flex w-full items-center gap-2 lg:gap-4">
+          <Button className="w-full justify-center">
             <EditIcon />
           </Button>
-          <Button className="bg-red-400 hover:bg-red-600">
+          <Button className="w-full justify-center bg-red-400 hover:bg-red-600">
             <CircleX />
           </Button>
         </div>
--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -2,9 +2,17 @@ import { useEffect, useState } from "react";
 import { getMovies } from "../../apis/moviesAPI";
 import Searchbar from "../../components/Searchbar";
 import MovieItem from "./MovieItem";
+import PaginationBar from "../../components/PaginationBar/PaginationBar";
 
 const Movies = () => {
   const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [moviesPerPage, setMoviesPerPage] = useState(3);
+
+  const lastIndex = currentPage * moviesPerPage; // 1 * 5
+  const firstIndex = lastIndex - moviesPerPage; // 5 -5
+  const currentMoviesPage = data.slice(firstIndex, lastIndex);
+  const npage = Math.ceil(data.length / moviesPerPage);
 
   const fetchMovies = async () => {
     try {
@@ -17,16 +25,27 @@ const Movies = () => {
 
   useEffect(() => {
     fetchMovies();
-  }, []);
+  }, [currentPage]);
 
-  console.log(data);
+  console.log(currentMoviesPage);
+
   return (
     <>
       <div className="flex">
         <Searchbar className="flex-grow md:flex-grow-0" />
       </div>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] gap-2 overflow-auto lg:gap-4">
-        {data && data.map((item) => <MovieItem key={item.maPhim} {...item} />)}
+        {data &&
+          currentMoviesPage.map((item) => (
+            <MovieItem key={item.maPhim} {...item} />
+          ))}
+      </div>
+      <div>
+        <PaginationBar
+          currentPage={currentPage}
+          totalPages={npage}
+          onSetCurrentPage={setCurrentPage}
+        />
       </div>
     </>
   );
